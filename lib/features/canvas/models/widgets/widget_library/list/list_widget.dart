4b7588f2f457bedec7958adc69812e6c960188fe
jellyie <okirynn@gmail.com>
2021-11-23 00:15:57 -0500

    Create folders for widget library

---
+++ lib/features/canvas/models/widgets/widget_library/list/list_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListWidget extends StatelessWidget {
+  const ListWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return const ListTile();
+      },
+    );
+  }
+}
